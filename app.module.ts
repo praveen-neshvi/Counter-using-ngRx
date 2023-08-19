7a344ec3f2f8059f3d82619e2eab59a3dca2b00c
praveen-neshvi <71161341+praveen-neshvi@users.noreply.github.com>
2023-08-19 15:02:46 +0530

    Adding ngRx effects to store counter to localStorage and console log

--- app.module.ts
+++ app.module.ts
@@ -6,6 +6,8 @@ import { CounterOutputComponent } from './counter-output/counter-output.componen
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './store/counter.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { CounterEffects } from './store/counter.effects';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { counterReducer } from './store/counter.reducer';
   ],
   imports: [BrowserModule, StoreModule.forRoot({
     counter: counterReducer
-  })],
+  }),
+  EffectsModule.forRoot([CounterEffects])],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ store/counter.effects.ts
@@ -0,0 +1,62 @@
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { decrement, increment } from "./counter.actions";
+
+import { tap, withLatestFrom } from "rxjs/operators"
+import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { countSelector } from "./counter.selector";
+
+@Injectable()
+export class CounterEffects {
+
+  constructor (private actions$ : Actions, private store : Store<{ counter: number}>) {}
+
+
+
+  saveCount = createEffect(() => this.actions$.pipe(
+    ofType(increment, decrement),
+    withLatestFrom(this.store.select(countSelector)),
+    tap(([action, counter]) => {
+      console.log(action)
+      if(action.type == '[Counter] Increment'){
+        localStorage.setItem('count', counter.toString())
+      } else if (action.type == '[Counter] Decrement'){
+        localStorage.setItem('count', counter.toString())
+      }
+    })
+  ),{dispatch : false}
+  )
+
+}
+
+//Effects are needed to handle such side effects because we can not use these side effects
+//such as HTTP requests in reducers as reducers need to be synchronous and their main core
+//job is to handle the state of store. Ergo use Effects insteads
+
+/*
+The actions$ is an observable that is injected from @ngrx/effects like this:
+  import { Actions } from ' @ngrx/effects '
+  constructor ( private actions$ : Actions ) { }
+
+This actions$ will always return a new value when any action is dispatched anywhere in the application.
+And we want a particular effect to happen when a particular action is dispatched and hence we can filter
+the required action using 'ofType' parameter inside actions$ object
+
+
+
+We do filter out the needed action using 'ofType' and decide what side effect should happen .
+The side effect to occur when some action is dispatched can also dispatch some other action and can also do
+something without dispatching another action.
+
+
+"withLatestFrom" operator is used to combine latest values coming from two different streams which emit different data.
+In this case the two different streams are:
+First, action$ : This stream emits actions dispatched within your application.
+Second, this.store.select : This is a stream that emits the current state of your application's store.
+
+
+when we use withLatestFrom, there will be two streams that are emitting data so the tap operator will
+receive an array of emitted values, which can be de-structured as
+tap ( ( [action (from action$), counter (from this.store.select)] ) => ...... )
+
+*/
